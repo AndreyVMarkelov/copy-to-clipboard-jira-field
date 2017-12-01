ecbe96134dc3359c24ecdb27e18c9960b466a8cd
amarkelov <andrey.markelov@infobip.com>
2017-12-01 08:27:13 +0300

    Up version&fix move bug

--- src/main/java/ru/andreymarkelov/atlas/plugins/copytoclipboard/field/CopyToClipboardCFType.java
+++ src/main/java/ru/andreymarkelov/atlas/plugins/copytoclipboard/field/CopyToClipboardCFType.java
@@ -4,6 +4,7 @@ import com.atlassian.jira.issue.Issue;
 import com.atlassian.jira.issue.customfields.impl.CalculatedCFType;
 import com.atlassian.jira.issue.customfields.impl.FieldValidationException;
 import com.atlassian.jira.issue.fields.CustomField;
+import com.atlassian.jira.issue.fields.config.FieldConfig;
 import com.atlassian.jira.issue.fields.config.FieldConfigItemType;
 import com.atlassian.jira.issue.fields.layout.field.FieldLayoutItem;
 import com.atlassian.templaterenderer.TemplateRenderer;
@@ -46,11 +47,15 @@ public class CopyToClipboardCFType extends CalculatedCFType<String, String> {
     @Override
     public String getValueFromIssue(CustomField customField, Issue issue) {
         if (issue != null) {
-            String pattern = copyToClipboardDataManager.getCopyPattern(customField.getRelevantConfig(issue));
-            return renderer.renderFragment(pattern, Collections.<String, Object>singletonMap("issue", issue));
-        } else {
-            return "";
+            FieldConfig fieldConfig = customField.getRelevantConfig(issue);
+            if (fieldConfig != null) {
+                return renderer.renderFragment(
+                        copyToClipboardDataManager.getCopyPattern(fieldConfig),
+                        Collections.<String, Object>singletonMap("issue", issue)
+                );
+            }
         }
+        return "";
     }
 
     public Map<String, Object> getVelocityParameters(Issue issue, CustomField customField, FieldLayoutItem fieldLayoutItem) {
