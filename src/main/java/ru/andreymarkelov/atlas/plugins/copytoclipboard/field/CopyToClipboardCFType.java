61b1c9874253f17ac240f0209efe5dd7984460e4
andreymarkelov <Chinus_(123)>
2019-09-11 16:39:33 +0300

    - Support Jira 8.4.x - Fix presence on agile boards

--- src/main/java/ru/andreymarkelov/atlas/plugins/copytoclipboard/field/CopyToClipboardCFType.java
+++ src/main/java/ru/andreymarkelov/atlas/plugins/copytoclipboard/field/CopyToClipboardCFType.java
@@ -2,31 +2,37 @@ package ru.andreymarkelov.atlas.plugins.copytoclipboard.field;
 
 import com.atlassian.jira.issue.CustomFieldManager;
 import com.atlassian.jira.issue.Issue;
-import com.atlassian.jira.issue.customfields.impl.CalculatedCFType;
 import com.atlassian.jira.issue.customfields.impl.FieldValidationException;
+import com.atlassian.jira.issue.customfields.impl.GenericTextCFType;
+import com.atlassian.jira.issue.customfields.manager.GenericConfigManager;
+import com.atlassian.jira.issue.customfields.persistence.CustomFieldValuePersister;
+import com.atlassian.jira.issue.customfields.view.CustomFieldParams;
 import com.atlassian.jira.issue.fields.CustomField;
 import com.atlassian.jira.issue.fields.config.FieldConfig;
 import com.atlassian.jira.issue.fields.config.FieldConfigItemType;
 import com.atlassian.jira.issue.fields.layout.field.FieldLayoutItem;
+import com.atlassian.jira.util.ErrorCollection;
 import com.atlassian.templaterenderer.TemplateRenderer;
 import ru.andreymarkelov.atlas.plugins.copytoclipboard.manager.CopyToClipboardDataManager;
 
 import javax.annotation.Nonnull;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import javax.annotation.Nullable;
+import java.util.*;
 
 import static org.apache.commons.lang3.StringEscapeUtils.unescapeHtml4;
 
-public class CopyToClipboardCFType extends CalculatedCFType<String, String> {
+public class CopyToClipboardCFType extends GenericTextCFType {
     private final CopyToClipboardDataManager copyToClipboardDataManager;
     private final TemplateRenderer renderer;
     private final CustomFieldManager customFieldManager;
 
     public CopyToClipboardCFType(
+            CustomFieldValuePersister customFieldValuePersister,
+            GenericConfigManager genericConfigManager,
             CopyToClipboardDataManager copyToClipboardDataManager,
             TemplateRenderer renderer,
             CustomFieldManager customFieldManager) {
+        super(customFieldValuePersister, genericConfigManager);
         this.copyToClipboardDataManager = copyToClipboardDataManager;
         this.renderer = renderer;
         this.customFieldManager = customFieldManager;
@@ -35,7 +41,7 @@ public class CopyToClipboardCFType extends CalculatedCFType<String, String> {
     @Override
     @Nonnull
     public List<FieldConfigItemType> getConfigurationItemTypes() {
-        List<FieldConfigItemType> configurationItemTypes = super.getConfigurationItemTypes();
+        List<FieldConfigItemType> configurationItemTypes = new ArrayList<>();
         configurationItemTypes.add(new CopyToClipboardCfConfig(copyToClipboardDataManager));
         return configurationItemTypes;
     }
@@ -48,6 +54,18 @@ public class CopyToClipboardCFType extends CalculatedCFType<String, String> {
     @Override
     public String getStringFromSingularObject(String s) {
         return s;
+    }
+
+    @Nullable
+    @Override
+    protected Object getDbValueFromObject(String s) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    protected String getObjectFromDbValue(@Nonnull Object o) throws FieldValidationException {
+        return null;
     }
 
     @Override
@@ -75,5 +93,56 @@ public class CopyToClipboardCFType extends CalculatedCFType<String, String> {
             }
         }
         return params;
+    }
+
+    @Override
+    public void createValue(CustomField field, Issue issue, @Nonnull String value) {
+    }
+
+    @Override
+    public void updateValue(CustomField customField, Issue issue, String value) {
+    }
+
+    @Override
+    public void setDefaultValue(FieldConfig fieldConfig, String value) {
+    }
+
+    @Override
+    public String getDefaultValue(FieldConfig fieldConfig) {
+        return null;
+    }
+
+    @Override
+    public String getChangelogValue(CustomField field, String value) {
+        return null;
+    }
+
+    @Override
+    public void validateFromParams(CustomFieldParams relevantParams, ErrorCollection errorCollectionToAddTo, FieldConfig config) {
+    }
+
+    @Override
+    public Set<Long> remove(CustomField field) {
+        return Collections.emptySet();
+    }
+
+    @Override
+    public String getValueFromCustomFieldParams(CustomFieldParams parameters) throws FieldValidationException {
+        Object firstValueForKey = parameters.getFirstValueForKey(null);
+        return this.getSingularObjectFromString((String) firstValueForKey);
+    }
+
+    @Override
+    public Object getStringValueFromCustomFieldParams(CustomFieldParams parameters) {
+        return parameters.getFirstValueForNullKey();
+    }
+
+    @Override
+    public Object accept(VisitorBase visitor) {
+        return visitor instanceof CopyToClipboardCFType.Visitor ? ((CopyToClipboardCFType.Visitor)visitor).visitCalculated(this) : super.accept(visitor);
+    }
+
+    public interface Visitor<X> extends VisitorBase<X> {
+        X visitCalculated(CopyToClipboardCFType cfType);
     }
 }
