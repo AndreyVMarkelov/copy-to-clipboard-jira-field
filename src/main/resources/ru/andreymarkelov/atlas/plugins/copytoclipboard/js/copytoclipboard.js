892252129ad88de97795a6088f98f383ecd0c465
Andrey Markelov <andrey.v.markelov@gmail.com>
2020-05-16 23:49:07 -0700

    Add tooltip

--- src/main/resources/ru/andreymarkelov/atlas/plugins/copytoclipboard/js/copytoclipboard.js
+++ src/main/resources/ru/andreymarkelov/atlas/plugins/copytoclipboard/js/copytoclipboard.js
@@ -1,7 +1,20 @@
 AJS.$().ready(function() {
-    var clipboard = new ClipboardJS(".copy-to-clipboard");
-    clipboard.on('error', function(e) {
-        console.error('Action:', e.action);
-        console.error('Trigger:', e.trigger);
+    var clipboardJSTrigger = function() {
+        var clipboard = new ClipboardJS(".copy-to-clipboard");
+        clipboard.on('error', function(e) {
+            console.error('Action:', e.action);
+            console.error('Trigger:', e.trigger);
+        });
+        $(".copy-to-clipboard").tooltip({trigger: "manual"});
+        $(".copy-to-clipboard").bind("click", function(event) {
+            $(".copy-to-clipboard").tooltip("show");
+            setTimeout(function() {
+                $(".copy-to-clipboard").tooltip("hide");
+            }, 1000);
+        });
+    }
+    JIRA.bind(JIRA.Events.NEW_CONTENT_ADDED, function (event, context, reason) {
+        clipboardJSTrigger();
     });
+    clipboardJSTrigger();
 });
